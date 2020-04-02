36ca67e8fc899de7aeb71f81d0be6076b8e29fae
Pavel Bobylyev <pbobylyev@appliedtech.ru>
2020-04-02 23:00:59 +0500

    vuex logux client added

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -1,9 +1,21 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import { createLogux } from '@logux/vuex';
+import { createLoguxCreator, badge, badgeEn, log } from '@logux/client';
+import { badgeStyles } from '@logux/client/badge/styles';
 
 Vue.use(Vuex);
 
-export default new Vuex.Store({
+const Logux = createLogux({
+  subprotocol: '1.0.0',
+  server: process.env.NODE_ENV === 'development'
+    ? 'ws://localhost:31337'
+    : 'wss://logux.example.com',
+  userId: false,  // TODO: We will fill it in next chapter
+  credentials: '' // TODO: We will fill it in next chapter
+})
+
+const store = new Logux.Store({
   state: {
   },
   mutations: {
@@ -13,3 +25,10 @@ export default new Vuex.Store({
   modules: {
   },
 });
+
+badge(store.client, { messages: badgeEn, styles: badgeStyles });
+log(store.client);
+
+store.client.start();
+
+export default store;
