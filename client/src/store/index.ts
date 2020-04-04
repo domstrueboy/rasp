644a24cf8494de5c60900f19abc6bae172bfefdc
Pavel Bobylyev <pbobylyev@appliedtech.ru>
2020-04-05 02:42:59 +0500

    server was changet to simple express one (with typescript though)

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -1,21 +1,9 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import { createLogux } from '@logux/vuex';
-import { badge, badgeEn, log } from '@logux/client';
-import { badgeStyles } from '@logux/client/badge/styles'; // eslint-disable-line
 
 Vue.use(Vuex);
 
-const Logux = createLogux({
-  subprotocol: '1.0.0',
-  server: process.env.NODE_ENV === 'development'
-    ? 'ws://localhost:31337'
-    : 'wss://logux.example.com',
-  userId: false, // TODO: We will fill it in next chapter
-  credentials: '', // TODO: We will fill it in next chapter
-});
-
-const store = new Logux.Store({
+export default new Vuex.Store({
   state: {
     count: 0,
   },
@@ -29,10 +17,3 @@ const store = new Logux.Store({
   modules: {
   },
 });
-
-badge(store.client, { messages: badgeEn, styles: badgeStyles });
-log(store.client);
-
-store.client.start();
-
-export default store;
